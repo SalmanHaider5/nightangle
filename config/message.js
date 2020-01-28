17e032d4bfbcadbe84e2ddd7b9e51eda1cb9b280
Salman Haider <salman.hadi98@gmail.com>
2020-01-28 21:24:29 +0500

    SMS Service integrated

--- config/message.js
+++ config/message.js
@@ -3,13 +3,17 @@ const { messages } = require('messagebird')('raoB0UJm6c4QyK5SRplg5qDjq')
 module.exports = (user, message) => {
     const params = {
         originator: 'Nightingale',
-        recepients: [
+        recipients: [
             user
         ],
         body: message
     }
     messages.create(params, (err, response) => {
-        if(err) return err
-        else return response
+        if(err){
+            return err
+        }
+        else{
+            return response
+        } 
     })
 }
