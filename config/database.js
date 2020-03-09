26445067d1c31a6bf0dec5659edcd0befbacb1a5
Salman Haider <salman.hadi98@gmail.com>
2020-03-09 22:21:59 +0500

    DB

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'admin',
-    PASSWORD: 'nightangle@20',
-    DB: 'nightangle',
+    HOST: 'us-cdbr-iron-east-04.cleardb.net',
+    USER: 'bd2d587960d031',
+    PASSWORD: '3589656d',
+    DB: 'heroku_ce895d85bb0ed18',
     DIALECT: 'mysql'
 }
