7489d7ad8fb524070aca87e539a5f84b079f0e0a
Salman Haider <salman.hayder112@gmail.com>
2020-03-23 18:55:35 +0500

    New Database added

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: 'us-cdbr-iron-east-04.cleardb.net',
-    USER: 'bddc387d4902fd',
-    PASSWORD: 'a4a2d578',
-    DB: 'heroku_ff73a726e63a34f',
+    HOST: 'us-cdbr-iron-east-01.cleardb.net',
+    USER: 'b8fe1361d33021',
+    PASSWORD: 'd2582f30',
+    DB: 'heroku_ba406798a28495b',
     DIALECT: 'mysql'
 }
