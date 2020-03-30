be43de7d19e24a4050c557764aa9481457df17e2
Salman Haider <salman.hayder112@gmail.com>
2020-03-30 22:49:47 +0500

    Database Changed

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: 'us-cdbr-iron-east-01.cleardb.net',
-    USER: 'bb46095416f3fc',
-    PASSWORD: '3f47f105',
-    DB: 'heroku_840524a44c5cbd7',
+    USER: 'b65c9837e15e16',
+    PASSWORD: '58861915',
+    DB: 'heroku_6885bb1a2f53701',
     DIALECT: 'mysql'
 }
