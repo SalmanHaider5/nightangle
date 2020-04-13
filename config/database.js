a42cb435126d30141cb773f605b072c85c60eb08
Salman Haider <salman.hayder112@gmail.com>
2020-04-13 19:53:45 +0500

    Test

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'nightingale',
+    HOST: 'us-cdbr-iron-east-01.cleardb.nethost',
+    USER: 'b0a3897fa06aa9',
+    PASSWORD: '5134746b',
+    DB: 'heroku_b59bd1962538d61',
     DIALECT: 'mysql'
 }
