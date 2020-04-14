1716d88c8d300a4e30dda79e39a0ce568671adf3
Salman Haider <salman.hayder112@gmail.com>
2020-04-14 22:43:40 +0500

    Test

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: 'us-cdbr-iron-east-01.cleardb.nethost',
+    HOST: 'us-cdbr-iron-east-01.cleardb.net',
     USER: 'b0a3897fa06aa9',
     PASSWORD: '5134746b',
     DB: 'heroku_b59bd1962538d61',
