07cb06d5d58733bb6882ddb234bcf9ef13d6d19e
Salman Haider <salman.hayder112@gmail.com>
2023-12-29 13:15:14 +0500

    Keys removed

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: 'us-cdbr-iron-east-04.cleardb.net',
-    USER: 'b462fa3a35faaa',
-    PASSWORD: 'be562d5a',
-    DB: 'heroku_d6b00899d5a8cac',
+    HOST: '',
+    USER: '',
+    PASSWORD: '',
+    DB: '',
     DIALECT: 'mysql'
 }
